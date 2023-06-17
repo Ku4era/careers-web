2e2d066a7aacf0a35c454d8db618c8112154b9c7
Ku4era <ku4era89@gmail.com>
2023-06-17 11:47:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'PM',
+  'location': 'Minsk',
+  'salary': 'BYN 5000'
+}, {
+  'id': 2,
+  'title': 'PO',
+  'location': 'Polotsk',
+}, {
+  'id': 3,
+  'title': 'SM',
+  'location': 'Remote',
+  'salary': 'BYN 6000',
+}, {
+  'id': 4,
+  'title': 'BA',
+  'location': 'Brest',
+  'salary': 'BYN 4500'
+}]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Vova')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
